f7c24a1d047fb5657210ea2af2b60ae1e8182967
flowdee <flowdee@users.noreply.github.com>
2019-03-29 18:50:50 +0100

    Added support for EDD HelpScout
    Output Quaderno Invoice URL inside HelpScout

--- includes/order_details.php
+++ includes/order_details.php
@@ -37,4 +37,35 @@ function edd_quaderno_add_payment_meta($payment_id) {
 }
 add_action('edd_view_order_details_payment_meta_before', 'edd_quaderno_add_payment_meta', 100);
 
+/**
+* Output Quaderno Invoice URL inside HelpScout
+*
+* Source: https://github.com/webzunft/edd-helpscout/blob/master/views/order-row.php#L38
+*
+* @param  array $order
+* @param  array $downloads
+* @return mixed|void
+ */
+function edd_quaderno_helpscout_widget_order_downloads( $order, $downloads ) {
+	
+	if ( ! isset( $order['payment_id'] ) )
+		return; 
+	
+	$payment = new EDD_Payment( $order['payment_id'] );
+	$quaderno_url = $payment->get_meta( '_quaderno_url' );
+
+	ob_start(); 
+	?>
+	<p>
+		<i class="icon-doc"></i><?php esc_html_e( 'Quaderno Invoice', 'edd-quaderno' ); ?>:&nbsp;
+		<?php if ( ! empty( $quaderno_url ) ) { ?>
+			<a href="<?php echo esc_url( $quaderno_url ); ?>" target="_blank" rel="nofollow"><?php esc_html_e( 'View', 'edd-quaderno' ); ?></a>
+		<?php } else { ?>
+			N/A
+		<?php } ?>
+	</p>
+	<?php
+	echo ob_get_clean();
+}
+add_action('edd_helpscout_before_order_downloads', 'edd_quaderno_helpscout_widget_order_downloads', 10, 2);
 ?>
