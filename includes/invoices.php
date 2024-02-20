991563740cce9cd0a71b23370f9d032884ed40d8
Carlos Hernandez <carlos@quaderno.io>
2024-02-20 23:25:37 +0000

    Use orders data to generate invoices

--- includes/invoices.php
+++ includes/invoices.php
@@ -15,84 +15,81 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 * Create invoice
 *
 * @since  1.0
-* @param  int $payment_id
+* @param  int $order_id
 * @return mixed|void
 */
-function edd_quaderno_create_invoice( $payment_id ) {
+function edd_quaderno_create_invoice( $order_id ) {
 	global $edd_options;
 
-	// Get the payment
-	$payment = new EDD_Payment($payment_id);
+	// Get the order
+	$order = edd_get_order($order_id);
 
 	// Return if the invoice total is zero
 	$skip = false; 
-	if ( $payment->total == 0 || apply_filters('quaderno_invoice_skip', $skip, $payment) ) {
+	if ( $order->total == 0 || apply_filters('quaderno_invoice_skip', $skip, $order) ) {
 		return;
 	}
 
-	// Return if an invoice has already been issued for this order
-	$transaction_id = $payment->get_meta( '_quaderno_invoice_id' );
-	if ( !empty( $transaction_id ) ) {
+	// Return if an order has already been issued for this order
+	if ( !empty( edd_quaderno_get_payment_meta( $order_id, '_quaderno_invoice_id' ) ) ) {
 		return;
 	}
 
-	// Get tax ID & business name
-	$tax_id = edd_quaderno_get_payment_meta( $payment_id, 'tax_id', true );
-	$business_name = edd_quaderno_get_payment_meta( $payment_id, 'business_name', true );
+	// Get the billing address, the tax ID & the business name
+	$address = $order->get_address();
+	$tax_id = edd_quaderno_get_payment_meta( $order_id, 'tax_id' );
+	$business_name = edd_quaderno_get_payment_meta( $order_id, 'business_name' );
 	
 	// Get the taxes
-	$tax = edd_quaderno_tax( $payment->address['country'], $payment->address['zip'], $payment->address['city'], $tax_id );
-	$tax = apply_filters( 'quaderno_invoice_tax', $tax, $payment );
+	$tax = edd_quaderno_tax( $address->country, $address->postal_code, $address->city, $tax_id );
+	$tax = apply_filters( 'quaderno_invoice_tax', $tax, $order );
 
 	// Add the invoice params
 	$transaction_params = array(
 		'type' => 'sale',
-		'currency' => $payment->currency,
-		'po_number' => $payment->number,
-		'interval_count' => $payment->parent_payment == 0 ? '0' : '1',
-		'notes' => apply_filters( 'quaderno_invoice_notes', $tax->notes, $payment, $tax ),
+		'currency' => $order->currency,
+		'po_number' => $order->number,
+		'interval_count' => $order->parent_payment == 0 ? '0' : '1',
+		'notes' => apply_filters( 'quaderno_invoice_notes', $tax->notes, $order, $tax ),
 		'processor' => 'edd',
-		'processor_id' => get_current_blog_id() . '_' . current_time('timestamp') . '_' . $payment_id,
+		'processor_id' => get_current_blog_id() . '_' . current_time('timestamp') . '_' . $order_id,
 		'payment' => array(
-			'method' => get_quaderno_payment_method( $payment->gateway ),
-      'processor' => $payment->gateway,
-      'processor_id' => $payment->transaction_id
+			'method' => get_quaderno_payment_method( $order->gateway ),
+      'processor' => $order->gateway,
+      'processor_id' => $order->transaction_id
 		),
 		'evidence' => array( 
-			'billing_country' => $payment->address['country'], 
-			'ip_address' => $payment->ip
+			'billing_country' => $address->country,
+			'ip_address' => $order->ip
 		),
 		'custom_metadata' => array( 
-			'processor_url' => add_query_arg( 'id', $payment_id, admin_url( 'edit.php?post_type=download&page=edd-payment-history&view=view-order-details' ) ) 
+			'processor_url' => add_query_arg( 'id', $order_id, admin_url( 'edit.php?post_type=download&page=edd-payment-history&view=view-order-details' ) )
 		)
 	);
 
 	// Add the contact
-	$customer = new EDD_Customer( $payment->customer_id );
+	$customer = edd_get_customer( $order->customer_id );
 	if ( !empty( $business_name ) ) {
 		$kind = 'company';
 		$first_name = $business_name;
-		$last_name = '';
-		$contact_name = implode( ' ', array($payment->first_name, $payment->last_name) );
+		$contact_name = $address->name;
 	} else {
 		$kind = 'person';
-		$first_name = $payment->first_name;
-		$last_name = $payment->last_name;
+		$first_name = $address->name;
 		$contact_name = '';
 	}
 
 	$transaction_params['customer'] = array(
 		'kind' => $kind,
 		'first_name' => $first_name ?: 'EDD Customer',
-		'last_name' => $last_name,
 		'contact_person' => $contact_name,
-		'street_line_1' => $payment->address['line1'] ?: '',
-		'street_line_2' => $payment->address['line2'] ?: '',
-		'city' => $payment->address['city'],
-		'postal_code' => $payment->address['zip'],
-		'region' => edd_get_state_name( $payment->address['country'], $payment->address['state'] ),
-		'country' => $payment->address['country'],
-		'email' => $payment->email,
+		'street_line_1' => $address->address ?: '',
+		'street_line_2' => $address->address2 ?: '',
+		'city' => $address->city,
+		'postal_code' => $address->postal_code,
+		'region' => edd_get_state_name( $address->country, $address->region ),
+		'country' => $address->country,
+		'email' => $order->email,
 		'tax_id' => $tax_id,
 		'processor' => 'edd'
 	);
@@ -104,35 +101,34 @@ function edd_quaderno_create_invoice( $payment_id ) {
   // - This is the first purchase ever for the user and the contact does not exist in Quaderno
   // - This is not the first purchase of the user but she does not have a contact_id saved yet in EDD metadata (until v1.24.3)
 
-  $payments = $customer->get_orders();
-  end($payments);
-  $last_payment = prev($payments);  
+  $orders = $customer->get_orders();
+  end($orders);
+  $last_order = prev($orders);
 
-  if(!empty($last_payment)){
-    $last_payment_business_name = edd_get_order_meta($last_payment->id, 'business_name');
+  if( !empty( $last_order ) ){
+    $last_order_business_name = edd_get_order_meta($last_order->id, 'business_name');
   }
   
   // If this is the customer's first purchase or their billing info has changed, then a new contact must be created
-  if (empty( $last_payment ) ||
-      $last_payment_business_name != $business_name ||
-      $last_payment->first_name != $payment->first_name ||
-      $last_payment->last_name != $payment->last_name){
+  if ( empty( $last_order ) ||
+      $last_order_business_name != $business_name ||
+      $last_order->address->name != $address->name ){
 
     // Force the creation of a new contact
-    $hashed_billing_name = md5(implode( '-', array($payment->first_name, $payment->last_name, $business_name)));
+    $hashed_billing_name = md5(implode( '-', array($address->name, $business_name)));
+
     // We don't want Quaderno to initialize the contact by processor_id
-    $transaction_params['customer']['processor_id'] = $hashed_billing_name . '_' . $payment->customer_id;
+    $transaction_params['customer']['processor_id'] = $hashed_billing_name . '_' . $order->customer_id;
   }
 
   if(!isset($transaction_params['customer']['processor_id'])){
     if ( empty( $contact_id ) ){
-      // Use the processor_id the already exists in Quaderno in order to identify the contact until we have contact_id stored in EDD
-      $transaction_params['customer']['processor_id'] = strtotime($customer->date_created) . '_' . $payment->customer_id;
+      // Use the processor_id that already exists in Quaderno in order to identify the contact until we have contact_id stored in EDD
+      $transaction_params['customer']['processor_id'] = strtotime($customer->date_created) . '_' . $order->customer_id;
     }else{
       // Use the contact_id to identify the contact
       $transaction_params['customer']['id'] = $contact_id;
       unset($transaction_params['customer']['first_name']);
-      unset($transaction_params['customer']['last_name']);
     }
   }
 	
@@ -140,36 +136,39 @@ function edd_quaderno_create_invoice( $payment_id ) {
 	$transaction = new QuadernoTransaction($transaction_params);
 
   // Calculate transaction items and tags
-	$tags = array();
 	$transaction_items = array();
-	foreach ( $payment->cart_details as $cart_item ) {
-		$download = new EDD_Download( $cart_item['id'] );
+	$tags = array();
+
+	foreach ( $order->items as $item ) {
+		$download = new EDD_Download( $item->product_id );
+		$sku = $download->get_sku();
 
 		// Calculate discount rate (if it exists)
-		$discount_rate = 0;
-		if ( $cart_item['discount'] > 0 ) {
-			$discount_rate = $cart_item['discount'] / $cart_item['subtotal'] * 100;
+    $discount_rate = 0;
+		if ( !empty( $item->discount )) {
+			$discount_rate = $item->discount / $item->subtotal * 100;
 		}
 
 		$new_item = array(
-			'product_code' => $download->post_name,
-			'description' => get_quaderno_payment_description( $cart_item, $payment->transaction_id ),
-			'quantity' => $cart_item['quantity'],
-			'amount' => $cart_item['subtotal'] + $cart_item['tax'] - $cart_item['discount'],
-			'discount_rate' => $discount_rate,
+			'product_code' => $sku != '-' ? $sku : '',
+			'description' => get_quaderno_item_description( $item ),
+			'quantity' => $item->quantity,
+			'amount' => $item->total,
+      'discount_rate' => $discount_rate,
 			'tax' => $tax
 		);
 
     array_push( $transaction_items, $new_item );
-		$tags = array_merge( $tags, wp_get_object_terms( $cart_item['id'], 'download_tag', array( 'fields' => 'slugs' ) ) );
+		$tags = array_merge( $tags, wp_get_object_terms( $item->id, 'download_tag', array( 'fields' => 'slugs' ) ) );
 	}
 
 	// Calculate gateway fees
-	foreach ( $payment->fees as $fee ) {
+	$fees = $order->get_fees();
+	foreach ( $fees as $fee ) {
 		$new_item = array(
-			'description' => $fee['label'],
+			'description' => $fee->description,
 			'quantity' => 1,
-			'amount' => $fee['amount'] * (1 + $tax->rate / 100.0),
+			'amount' => $fee->subtotal * ( 1 + $tax->rate / 100.0 ),
 			'tax' => $tax
 		);
 
@@ -190,15 +189,17 @@ function edd_quaderno_create_invoice( $payment_id ) {
 	 * @param \QuadernoTransaction $transaction The transaction object.
 	 * @param \EDD_Payment         $payment     The EDD payment object.
 	 */
-	$transaction = apply_filters( 'quaderno_invoice_transaction', $transaction, $payment );
+	$transaction = apply_filters( 'quaderno_invoice_transaction', $transaction, $order );
 
-	do_action( 'quaderno_invoice_pre_create', $transaction, $payment );
+	do_action( 'quaderno_invoice_pre_create', $transaction, $order );
 
 	// Save the invoice and the location evidences
 	if ( $transaction->save() ) {
-    $payment->update_meta( '_quaderno_invoice_id', $transaction->id );
-    $payment->update_meta( '_quaderno_processor_id', $transaction->processor_id );
-		$payment->update_meta( '_quaderno_url', $transaction->permalink );
+		edd_update_order_meta( $order->id, '_quaderno_invoice_id', $transaction->id );
+		edd_update_order_meta( $order->id, '_quaderno_processor_id', $transaction->processor_id );
+		edd_update_order_meta( $order->id, '_quaderno_url', $transaction->permalink );
+
+		$payment = edd_get_payment( $order_id );
 		$payment->add_note( 'Receipt created on Quaderno' );
 
     $customer->update_meta( '_quaderno_contact', $transaction->contact->id );
@@ -227,8 +228,8 @@ add_action( 'edd_complete_purchase', 'edd_quaderno_create_invoice', 999 );
 function edd_quaderno_process_recurring_payment( $order_id, $parent_id ) {
 	global $edd_options;
 
-	$payment = new EDD_Payment($order_id);
-	$customer = new EDD_Customer( $payment->customer_id );
+	$order = edd_get_order($order_id);
+	$customer = edd_get_customer( $order->customer_id );
 
 	// get the tax ID and the business name from the customer's current data
 	edd_add_order_meta( $order_id, 'tax_id', $customer->get_meta( 'tax_id' ) );
@@ -238,6 +239,17 @@ function edd_quaderno_process_recurring_payment( $order_id, $parent_id ) {
 	$parent_order = edd_get_order( $parent_id );
 	if ( !empty($parent_order) ) {
 		edd_update_order( $order_id, array( 'ip' => $parent_order->ip ) );
+		$result = edd_update_order_address(
+			$order->get_address()->id,
+			array(
+				'address'     => $parent_order->address->address,
+				'address2'    => $parent_order->address->address2,
+				'city'        => $parent_order->address->city,
+				'region'      => $parent_order->address->region,
+				'postal_code' => $parent_order->address->postal_code,
+				'country'     => $parent_order->address->country
+			)
+		);
 	}
 
 	// generate the invoice
@@ -260,7 +272,7 @@ function edd_quaderno_resend_invoice( $data ) {
 	}
 
 	$payment = new EDD_Payment($payment_id);
-	edd_quaderno_process_recurring_payment( $payment_id, $payment->parent_payment );
+	edd_quaderno_process_recurring_payment( $payment_id, $order->parent_payment );
 
 	wp_redirect( add_query_arg( array( 'edd-message' => 'email_sent', 'edd-action' => false, 'purchase_id' => false ) ) );
 	exit;
--- includes/payments.php
+++ includes/payments.php
@@ -40,29 +40,33 @@ function get_quaderno_payment_method( $gateway ) {
 }
 
 /**
- * Get payment description for Quaderno from a cart item
+ * Get item description for Quaderno from an order item
  *
  * @param $item
- * @param $transaction_id
  */
-function get_quaderno_payment_description( $item, $transaction_id ) {
-  $download = new EDD_Download( $item['id'] );
-  $product_name = $download->post_title;
+function get_quaderno_item_description( $order_item ) {
+  $download = edd_get_download( $order_item->product_id );
+  $name = $download->get_name();
 
   // Check if the item is a product variant
-  $price_id = edd_get_cart_item_price_id( $item );
-  if ( edd_has_variable_prices( $item['id'] ) && ! is_null( $price_id ) ) {
-    $product_name .= ' - ' . edd_get_price_option_name( $item['id'], $price_id, $transaction_id );
+  if ( $download->has_variable_prices() && ! empty( $order_item->price_id ) ) {
+    $variation_name = edd_get_price_option_name( $download->ID, $order_item->price_id );
+    if ( ! empty( $variation_name ) ) {
+      $name .= ' - ' . $variation_name;
+    }
   }
 
   // Check if the item is an ugrade or a renewal (for recurring payments)
-  if ( ! empty( $item['item_number']['options']['is_upgrade'] ) ) {
-    $product_name .= sprintf(' (%s)', esc_html__('Upgrade'));
-  } elseif ( ! empty( $item['item_number']['options']['is_renewal'] ) ) {
-    $product_name .= sprintf(' (%s)', esc_html__('Renewal'));
+  $payment   = edd_get_payment( $order_item->order_id );
+  $cart_item = $payment->cart_details[$order_item->cart_index];
+
+  if ( ! empty( $cart_item['item_number']['options']['is_upgrade'] ) ) {
+    $name .= sprintf(' (%s)', esc_html__('Upgrade'));
+  } elseif ( ! empty( $cart_item['item_number']['options']['is_renewal'] ) ) {
+    $name .= sprintf(' (%s)', esc_html__('Renewal'));
   }
 
-  return apply_filters( 'quaderno_payment_description', $product_name, $item );
+  return apply_filters( 'quaderno_payment_description', $name, $order_item );
 }
 
 ?>
